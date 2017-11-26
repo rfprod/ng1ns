9a45f80f3262180a980b4a0faae0dd778436c22d
rfprod <zoidenmacht@zoho.com>
2017-11-26 09:15:53 -0800

    (fix) run-server.sh, package scripts

--- run-server.sh
+++ run-server.sh
@@ -1,20 +1,5 @@
 #!/bin/bash
-NAME=`whoami`
 
 pkill -f http-server
 
-#
-#	TODO
-#	replace "ruser" with proper user name for development environment
-#
-if [ $NAME = "ruser" ];then
-	echo "development env"
-	npx http-server -a localhost -p 7070 -c-1 ./app
-else
-	echo "production env"
-	#
-	#	TODO
-	#	replace 127.0.0.1 with proper IP address
-	#
-	npx http-server -a 127.0.0.1 -p 7070 -c-1 ./app
-fi
+npx http-server -a localhost -p 7070 -c-1 ./app
